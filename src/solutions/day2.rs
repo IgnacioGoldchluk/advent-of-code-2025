c1ae9d3ba233f17db77b0f6e85c791fba1db89ee
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2025-12-02 12:48:00 +0200

    day2 part2

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -1,6 +1,7 @@
 use crate::solutions::solution;
-use std::cmp;
+use std::{cmp, collections::HashSet};
 
+#[derive(Debug)]
 struct IdRange {
     low: String,
     high: String,
@@ -11,13 +12,28 @@ impl solution::Solver for Day2Solver {
     fn solve(&self, input: &str) -> solution::Solution {
         solution::Solution {
             part1: part1(input),
-            part2: "0".into(),
+            part2: part2(input),
         }
     }
 }
 
+fn part2(input: &str) -> String {
+    let total: u64 = input
+        .split(",")
+        .map(to_range)
+        .map(|range| invalid_ids_sum_2(range))
+        .sum();
+
+    total.to_string()
+}
+
 fn part1(input: &str) -> String {
-    let total: u64 = input.split(",").map(to_range).map(invalid_ids_sum).sum();
+    let repetitions = 2;
+    let total: u64 = input
+        .split(",")
+        .map(to_range)
+        .map(|range| invalid_ids_sum(&range, repetitions))
+        .sum();
 
     total.to_string()
 }
@@ -34,30 +50,43 @@ fn to_range(range: &str) -> IdRange {
     }
 }
 
-fn invalid_ids_sum(range: IdRange) -> u64 {
+fn invalid_ids_sum_2(range: IdRange) -> u64 {
+    (2..=range.high.len())
+        .flat_map(|rep| invalid_ids(&range, rep))
+        .collect::<HashSet<u64>>()
+        .iter()
+        .sum()
+}
+
+fn invalid_ids(range: &IdRange, repetitions: usize) -> Vec<u64> {
+    let high: u64 = range.high.parse().unwrap();
+    (first_invalid_id_after(&range.low, repetitions)..)
+        .map(|n| generate_invalid_id(n, repetitions))
+        .take_while(|n| *n <= high)
+        .collect()
+}
+
+fn invalid_ids_sum(range: &IdRange, repetitions: usize) -> u64 {
     // Invalid IDs are duplicate sequences of digits
     // "99", "1212", "123123", "6565", etc.
     // The fastest way is to construct the ids starting from the smallest one
     // greater than the lowest range, increment by one and collect until we exceed
     // the highest range
-    let high: u64 = range.high.parse().unwrap();
-
-    (first_invalid_id_after(&range.low)..)
-        .map(generate_invalid_id)
-        .take_while(|n| *n <= high)
-        .collect::<Vec<u64>>()
-        .iter()
-        .sum()
+    invalid_ids(range, repetitions).iter().sum()
 }
 
-fn first_invalid_id_after(number: &str) -> u64 {
+fn first_invalid_id_after(number: &str, repetitions: usize) -> u64 {
     // We need to start with a 1 followed by zeroes. The number
-    // of zeroes is (nl / 2) - 1, where `nl` is the length of the number. This
-    // is because we need repeated patterns twice (hence dividing by 2) and
+    // of zeroes is (nl / repetitions) - 1, where `nl` is the length of the number. This
+    // is because we need repeated patterns twice (hence dividing by repetitions) and
     // one of the digits is reserved for the first `1`
     let nl = number.len();
-    let length = if nl.is_multiple_of(2) { nl } else { nl + 1 };
-    let invalid_num: u64 = ("1".to_string() + &"0".repeat(cmp::max(0, (length / 2) - 1)))
+    let length = if nl.is_multiple_of(repetitions) {
+        nl
+    } else {
+        nl + 1
+    };
+    let invalid_num: u64 = ("1".to_string() + &"0".repeat(cmp::max(0, (length / repetitions) - 1)))
         .parse()
         .unwrap();
 
@@ -65,13 +94,12 @@ fn first_invalid_id_after(number: &str) -> u64 {
 
     (invalid_num..)
         .into_iter()
-        .find(|n| generate_invalid_id(*n) >= start)
+        .find(|n| generate_invalid_id(*n, repetitions) >= start)
         .unwrap()
 }
 
-fn generate_invalid_id(number: u64) -> u64 {
-    // This could also be done by (number + (number*int(log10(number))))
-    number.to_string().repeat(2).parse().unwrap()
+fn generate_invalid_id(number: u64, repetitions: usize) -> u64 {
+    number.to_string().repeat(repetitions).parse().unwrap()
 }
 
 #[cfg(test)]
@@ -86,6 +114,6 @@ mod tests {
 
         let solution = Day2Solver.solve(input);
         assert_eq!(solution.part1, "1227775554");
-        assert_eq!(solution.part2, "0");
+        assert_eq!(solution.part2, "4174379265");
     }
 }
