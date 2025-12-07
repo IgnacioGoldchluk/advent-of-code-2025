c5721004b90276ef0799feb3451eec8968b092f9
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2025-12-07 19:46:03 +0200

    day7 part1

---
+++ src/solutions/day7.rs
@@ -0,0 +1,123 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Error,
+};
+
+use crate::solutions::solution;
+
+pub struct Day7Solver;
+
+impl solution::Solver for Day7Solver {
+    fn solve(&self, input: &str) -> solution::Solution {
+        solution::Solution {
+            part1: part1(input).to_string(),
+            part2: "".into(),
+        }
+    }
+}
+
+enum Point {
+    Space,
+    Splitter,
+    Beam,
+}
+
+type Coord = (usize, usize);
+type Grid = HashMap<Coord, Point>;
+
+fn part1(input: &str) -> u64 {
+    let grid = parse(input);
+
+    let initial_beam = find_beam(&grid);
+    let mut beams = HashSet::from([initial_beam]);
+    let mut splits = 0;
+
+    while !beams.is_empty() {
+        // Get the first element by x,y coordinate
+        let mut p: Vec<&Coord> = beams.iter().collect();
+        p.sort();
+        let beam_point = *p[0];
+        beams.remove(&beam_point);
+        let next_point = (beam_point.0 + 1, beam_point.1);
+        match grid.get(&next_point) {
+            None => (),
+            Some(Point::Beam) => panic!("Unexpected beam"),
+            Some(Point::Space) => {
+                beams.insert(next_point);
+            }
+            Some(Point::Splitter) => {
+                splits += 1;
+                let (left, right) = split_point(&next_point);
+                beams.insert(left);
+                beams.insert(right);
+            }
+        }
+    }
+
+    splits
+}
+
+fn split_point(point: &Coord) -> (Coord, Coord) {
+    ((point.0, point.1 - 1), (point.0, point.1 + 1))
+}
+
+fn find_beam(grid: &Grid) -> Coord {
+    grid.iter()
+        .find_map(|(coord, point)| match point {
+            Point::Beam => Some(*coord),
+            _ => None,
+        })
+        .unwrap()
+}
+
+fn parse(input: &str) -> Grid {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(row_idx, row)| {
+            row.chars()
+                .enumerate()
+                .map(move |(col_idx, c)| ((row_idx, col_idx), Point::try_from(c).unwrap()))
+        })
+        .collect()
+}
+
+impl TryFrom<char> for Point {
+    type Error = Error;
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '.' => Ok(Point::Space),
+            'S' => Ok(Point::Beam),
+            '^' => Ok(Point::Splitter),
+            _ => Err(Error),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::solution::Solver;
+    use crate::solutions::day7::Day7Solver;
+
+    #[test]
+    fn test_input() {
+        let input = r#".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+..............."#;
+        let solution = Day7Solver.solve(input);
+        assert_eq!(solution.part1, "21");
+    }
+}
