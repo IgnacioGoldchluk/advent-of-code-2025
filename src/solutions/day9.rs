1d980e63e87d26032bbae57603953fc8dea3311e
IgnacioGoldchluk <goldchlukignacio@gmail.com>
2025-12-09 12:45:02 +0200

    day9 part2

--- src/solutions/day9.rs
+++ src/solutions/day9.rs
@@ -6,9 +6,44 @@ impl solution::Solver for Day9Solver {
     fn solve(&self, input: &str) -> solution::Solution {
         solution::Solution {
             part1: part1(input).to_string(),
-            part2: "".into(),
+            part2: part2(input).to_string(),
         }
     }
+}
+
+fn part2(input: &str) -> u64 {
+    let coords: Vec<Coord> = input.lines().map(Coord::from).collect();
+    let length = coords.len();
+    let rectangles = rectangles(&coords);
+
+    let (p1, p2) = rectangles
+        .iter()
+        .find(|(p1, p2)| {
+            let (xmin, xmax, ymin, ymax) = edges(p1, p2);
+            for (i, c1) in coords.iter().enumerate() {
+                let c2 = &coords[(i + 1) % length];
+
+                if c1.0 == c2.0 {
+                    // Horizontal line,
+                    let (ylmin, ylmax) = (c1.1.min(c2.1), c1.1.max(c2.1));
+                    if xmin < c1.0 && xmax > c1.0 && ymin < ylmax && ymax > ylmin {
+                        return false;
+                    }
+                } else if c1.1 == c2.1 {
+                    // Vertical line
+                    let (xlmin, xlmax) = (c1.0.min(c2.0), c1.0.max(c2.0));
+                    if ymin < c1.1 && ymax > c1.1 && xmin < xlmax && xmax > xlmin {
+                        return false;
+                    }
+                } else {
+                    panic!("Unreachable");
+                }
+            }
+            true
+        })
+        .unwrap();
+
+    p1.area(p2)
 }
 
 fn part1(input: &str) -> u64 {
@@ -24,6 +59,29 @@ fn part1(input: &str) -> u64 {
     max_val
 }
 
+fn edges(point1: &Coord, point2: &Coord) -> (u64, u64, u64, u64) {
+    (
+        point1.0.min(point2.0),
+        point1.0.max(point2.0),
+        point1.1.min(point2.1),
+        point1.1.max(point2.1),
+    )
+}
+
+fn rectangles(coords: &[Coord]) -> Vec<(Coord, Coord)> {
+    let mut pairs = vec![];
+    for i in 0..(coords.len() - 1) {
+        for j in i + 1..coords.len() {
+            pairs.push((coords[i].clone(), coords[j].clone()))
+        }
+    }
+
+    pairs.sort_by(|p1, p2| p1.0.area(&p1.1).cmp(&p2.0.area(&p2.1)));
+    pairs.reverse();
+    pairs
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
 struct Coord(u64, u64);
 
 impl From<&str> for Coord {
@@ -59,5 +117,6 @@ mod tests {
 7,3"#;
         let solution = Day9Solver.solve(input);
         assert_eq!(solution.part1, "50");
+        assert_eq!(solution.part2, "24");
     }
 }
